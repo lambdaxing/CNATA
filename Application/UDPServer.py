04f7c5e73402846a68fa3978cd032d0a703ecca1
lambdaxing <lambdaxing@foxmail.com>
2022-02-20 16:20:04 +0800

    debug...

--- Application/UDPServer.py
+++ Application/UDPServer.py
@@ -3,7 +3,7 @@ serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
-while true:
+while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
