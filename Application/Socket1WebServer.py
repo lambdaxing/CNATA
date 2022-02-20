10a24aa4e6587539c579caa4bd1928fc1e963cfd
lambdaxing <lambdaxing@foxmail.com>
2022-02-20 20:01:53 +0800

    debug

--- Application/Socket1WebServer.py
+++ Application/Socket1WebServer.py
@@ -6,22 +6,22 @@ serverPort = 80
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 while True:
-     #Establish the connection
-     print('Ready to serve...')
-     connectionSocket, addr = serverSocket.accept()
-     try:
-         message = connectionSocket.recv(1024)
-         filename = message.split()[1]
-         f = open(filename[1:])
-         outputdata = f.read()
-         f.close()
-         #Send one HTTP header line into socket
-         outputdata = 'HTTP/1.1 200 OK\r\n\r\n' + outputdata
-         #Send the content of the requested file to the client
-         for i in range(0, len(outputdata)):
-             connectionSocket.send(outputdata[i].encode())
-         connectionSocket.close()
-         print("OK!")
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        f.close()
+        #Send one HTTP header line into socket
+        outputdata = 'HTTP/1.1 200 OK\r\n\r\n' + outputdata
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+        print("OK!")
     except IOError:
         #Send response message for file not found
         outputdata = 'HTTP/1.1 404 Not Found\r\n\r\n'
