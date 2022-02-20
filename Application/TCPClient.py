ee83754b79444df8232c6ade439929982d752fad
lambdaxing <lambdaxing@foxmail.com>
2022-02-20 16:48:14 +0800

    tcp socket program

---
+++ Application/TCPClient.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = '112.74.41.43'
+serverPort = 12001
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = raw_input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
